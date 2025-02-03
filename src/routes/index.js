f699e4a62745988ad0d70f0717a963d34ef06a02
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-02-03 13:14:13 -0600

    importando y montando authRoutes.js en index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoutes from "./userRoutes.js"; // importamos las rutas del usuario
-
+import authRoutes from "./authRoutes.js"
 const router = express.Router();
 
 //Colocamos las rutas del usuario bajo /users
 router.use("/users", userRoutes);
+// rutas de autenticacion
+router.use("/auth", authRoutes);
 
 // ruta de RAIZ
 router.get("/",(req, res)=>{

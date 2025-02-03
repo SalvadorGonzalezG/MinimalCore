539fbe1c89670e60e021d55498180d5e0581d668
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-02-03 01:20:02 -0600

    inicializar proyecto con Node.js y Express.- Configuración inicial de Express.- Agregar estructura básica de carpetas (controllers, routes, middlewares). - Configurar scripts de npm y variables de entorno.

---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+// Logica para las rutas de usuarios
+
+export const getUsers = (req, res)=>{
+    res.json({message: "List To Users:"})
+
+}
+
+// Creamos un nuevo usuario
+export const createUser = (req, res)=>{
+    const user = req.body;
+    res.status(201).json({message: "Usuario Creado", user})
+    
+}
